5d34f7965b131faf511c4c4e44a4888f03e3f32c
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-06-28 00:27:41 -0500

    Create models.py

---
+++ follows/models.py
@@ -0,0 +1,3 @@
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(CustomUser, related_name='followers', on_delete=models.CASCADE)
