0ec8768a7e43411be2c06192d04b26ec2ce877ae
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-06-28 00:24:58 -0500

    Create models.py

---
+++ posts/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from users.models import CustomUser
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
